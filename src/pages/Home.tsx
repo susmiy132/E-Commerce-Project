4327e6ea3902f1740c100330fffa74b36f1d81a8
susmiy132 <thakursusmi94@gmail.com>
2026-06-23 17:55:31 +0545

    responsive home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,39 @@
 import React from "react";
+import Banner from "../assets/banner.png";
 
 
-function Home() {
+export default function Home() {
     return (
-        <div className="items-center justify-center font-extrabold font-primary">
-            This is Home pages
-        </div>
+        <>
+            <div className=""></div>
+            <div className="relative">
+                <img src={Banner} className="w-full" alt="" />
+                <div className="absolute top-1/2 left-0 w-full" style={{ transform: "translateY(-50%)" }}>
+                    <div className="container pl-15">
+                        <p className="font-josefin font-bold text-[24px] sm:text-[24px] md:text-[32px] lg:text-[40px] xl:text-[48px] 2xl:text-[56px]">
+                            New Furniture Collection
+                        </p>
+
+                        <p className="font-josefin font-bold text-[24px] md:text[32px] sm:text[24px] lg:text-[40px] xl:text-[48px] 2xl:text-[56px]">
+                            Trends in 2020
+                        </p>
+
+                    </div>
+
+                </div>
+            </div>
+
+            <div>
+                <div className="grid grid-cols-4 container">
+                    {[1, 2, 3, 4].map((el) => {
+                        return (
+                            <div className="border p-5">
+                                <p>Products</p>
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+        </>
     );
 }
-
-export default Home;
