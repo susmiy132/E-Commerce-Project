ab29405b2358fee5132a2fb71f3523dc150527f6
susmiy132 <thakursusmi94@gmail.com>
2026-07-08 19:32:41 +0545

    protect admin routes

--- src/components/layouts/ProtectedRoute.tsx
+++ src/components/layouts/ProtectedRoute.tsx
@@ -3,18 +3,36 @@ import { useSelector } from "react-redux";
 import { Navigate, Outlet, Router, useNavigate } from "react-router";
 import type { RootState } from "../../redux/store";
 
-function ProtectedRoute() {
-    const user = useSelector((state: RootState) => state.user.value);
-    //   const navigate = useNavigate();
 
+function ProtectedRoute({
+    forSeller,
+    forAdmin,
+}: {
+    forSeller?: boolean;
+    forAdmin?: boolean;
+}) {
+    const user = useSelector((state: RootState) => state.user.value);
     if (user) {
+        if (forSeller) {
+            if (user.isSeller) {
+                return <Outlet />;
+            } else {
+                return <Navigate to={"/forbidden"} replace />;
+
+            }
+        }
+
+        if (forAdmin) {
+            if (user.isAdmin) {
+                return <Outlet />;
+            } else {
+                return <Navigate to={"/forbidden"} />;
+            }
+        }
         return <Outlet />;
     } else {
-
-        // navigate("/login"); // not working
-        return <Navigate to={"/login"} replace />; // feels like glitch
+        return <Navigate to={"/login"} replace />;
     }
-
 }
 
 export default ProtectedRoute;
