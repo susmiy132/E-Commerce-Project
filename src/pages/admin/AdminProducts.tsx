ae861f6b27665095bcaa2dc5978d9d682159f7db
susmiy132 <thakursusmi94@gmail.com>
2026-07-08 19:31:04 +0545

    add admin products page

---
+++ src/pages/admin/AdminProducts.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function AdminProducts() {
+    // useEffect(() =>{
+    //     {{base_url}}/api/admin/products
+    // },[])
+    return (
+        <div>AdminProducts</div>
+    )
+}
+
+export default AdminProducts
